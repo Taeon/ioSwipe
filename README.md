1aa520f9fd201266012736785f441e5c4b791497
Taeon <github-taeon@filofox.com>
2018-01-01 16:19:51 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ioSwipe
-Quickly add support for touchscreen swipe events
+Quickly add support for touchscreen swipe events to HTML/JavaScript.
+
+## Usage
+
+Include the code, then simply call ioSwipe and pass in the element(s) you want to emit swipe events. You can pass in a jQuery-style selector string*, a single HTML element, or a collection of elements (e.g. a jQuery collection or the result of a call to ```document.querySelectorAll()```):
+
+```javascript
+ioswipe( '#myElement' ); // Selector string
+ioswipe( document.getElementById( 'myElement' ) ); // A single element
+ioswipe( $( '#myElement' ) ); // A jQuery collection
+ioswipe( document.querySelector( '#myElement' ) ); // A single element
+ioswipe( document.querySelectorAll( '.my-element-class' ) ); // A collection of elements
+```
+
+\* in fact, internally it uses ```querySelectorAll``` so if you're using jQuery and are worried about any possible differences in behaviour, just pass in a jQuery collection instead.
+
+Now you can listen for swipe events. Two events are emitted -- firstly, a generic swipe event (```swipe```) and a separate event for the direction of the swipe (```swipeleft```, ```swiperight```, ```swipeup```, ```swipedown```). So you can listen for any swipe, or just one or more specific directions:
+
+```javascript
+// Native event handling
+document.getElementById( 'myElement' ).addEventListener(
+    'swipe', // A swipe occurred
+    function( event ){
+        // The direction of the swipe is in the event detail object
+        alert( 'Swipe direction: ' + event.detail.direction );
+    }
+);
+// With jQuery
+$( 'myElement' ).on( 'swipeleft', function(){alert( 'Left swipe occurred' )} );
+```
---
+++ src/js/ioswipe.js
@@ -0,0 +1,133 @@
+/**
+ * ioSwipe
+ *
+ * Instant support for swipe events
+ *
+ * Based on code from //https://codepen.io/ganmahmud/pen/RaoKZa
+ *
+ * See https://github.com/Taeon/ioSwipe for docs
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Patrick Fox
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof exports === "object") {
+        module.exports = factory();
+    } else {
+        root.ioswipe = factory();
+    }
+}(
+    this,
+    function () {
+
+        var Swipe = function( el, dir ){
+            var data = {direction: dir};
+            if (window.CustomEvent) {
+                // Generic swipe event
+                var swipe_event = new CustomEvent('swipe', {detail: data});
+                // Swipe direction
+                var swipe_dir_event = new CustomEvent('swipe' + dir, {detail: data});
+            } else {
+                // Generic swipe event
+                var swipe_event = document.createEvent('CustomEvent');
+                swipe_event.initCustomEvent('swipe', true, true, data);
+                // Swipe direction
+                var swipe_dir_event = document.createEvent('CustomEvent');
+                swipe_dir_event.initCustomEvent('swipe' + dir, true, true, data);
+            }
+
+            el.dispatchEvent(swipe_event);
+            el.dispatchEvent(swipe_dir_event);
+        }
+
+        var EnableSwipe = function( el ){
+
+            var touchsurface = el,
+            swipedir,
+            startX,
+            startY,
+            distX,
+            distY,
+            threshold = 150, //required min distance traveled to be considered swipe
+            restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+            allowedTime = 300, // maximum time allowed to travel that distance
+            elapsedTime,
+            startTime
+
+            touchsurface.addEventListener('touchstart', function(e){
+                var touchobj = e.changedTouches[0]
+                swipedir = 'none'
+                dist = 0
+                startX = touchobj.pageX
+                startY = touchobj.pageY
+                startTime = new Date().getTime() // record time when finger first makes contact with surface
+                e.preventDefault()
+            }, false)
+
+            touchsurface.addEventListener('touchmove', function(e){
+                e.preventDefault() // prevent scrolling when inside DIV
+            }, false)
+
+            touchsurface.addEventListener('touchend', function(e){
+                var touchobj = e.changedTouches[0]
+                distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+                distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+                elapsedTime = new Date().getTime() - startTime // get time elapsed
+                if (elapsedTime <= allowedTime){ // first condition for swipe met
+                    if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                        swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+                    }
+                    else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                        swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+                    }
+                }
+                Swipe(el,swipedir);
+                e.preventDefault();
+            }, false)
+        }
+
+        var ioswipe = function( el ){
+            switch( typeof el ){
+                case 'object':{
+                    if( typeof el.length != 'number' ){
+                        // A single element e.g. result of document.querySelector()
+                        // or document.getElementById())
+                        el = [ el ];
+                    }
+                    break;
+                }
+                case 'string':{
+                    // A selector string
+                    el = document.querySelectorAll( el );
+                    break;
+                }
+            }
+            for( var i = 0; i < el.length; i++ ){
+                EnableSwipe( el[ i ] );
+            }
+        }
+        return ioswipe;
+    }
+)
+);
